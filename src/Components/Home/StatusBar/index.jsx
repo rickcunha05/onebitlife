50b4006c50d9b11b5bed2ad60af60a0ce1466a05
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2022-12-04 00:17:06 -0300

    StatusBar finished and implemented paper

---
+++ src/Components/Home/StatusBar/index.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { View, StyleSheet, Image } from "react-native";
+
+import { ProgressBar } from "react-native-paper";
+
+export default function StatusBar(){
+    return(
+        <View style={styles.container}>
+
+            {/* Inicio Statusbar education */}
+            <View style={styles.StatusBarContainer}>
+            <Image 
+                    source={require('../../../assets/icons/educationIcon.png')}
+                    style={styles.icon}
+                />
+                <ProgressBar progress={1} color={"#90B7f3"} style={styles.progress}/>
+            </View>
+
+             {/* Inicio Statusbar finance */}
+
+            <View style={styles.StatusBarContainer}>
+            <Image 
+                    source={require('../../../assets/icons/moneyIcon.png')}
+                    style={styles.icon}
+                />
+                <ProgressBar progress={1} color={"#85bb65"} style={styles.progress}/>
+            </View>
+
+             {/* Inicio Statusbar body */}
+
+            <View style={styles.StatusBarContainer}>
+            <Image 
+                    source={require('../../../assets/icons/bodyIcon.png')}
+                    style={styles.icon}
+                />
+                <ProgressBar progress={1} color={"#FF0044"} style={styles.progress}/>
+            </View>
+             
+             {/* Inicio Statusbar fun */}
+
+            <View style={styles.StatusBarContainer}>
+            <Image 
+                    source={require('../../../assets/icons/funIcon.png')}
+                    style={styles.icon}
+                />
+                <ProgressBar progress={1} color={"#FE7F23"} style={styles.progress}/>
+            </View>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        padding: 20,
+        backgroundColor:"#151515",       
+        borderRadius: 10,
+        marginTop: 10,
+        marginBottom: 10        
+    },statusBarContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginVertical: 6,        
+      },
+      progress: {
+        borderRadius: 10,
+        width: 250,
+        height: 8,
+      },
+      icon: {
+        width: 25,
+        height: 25,        
+        marginTop: 10,
+        marginRight: 5,
+      },
+})
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { useNavigation } from "@react-navigation/native"
 import { ScrollView, Text, View, StyleSheet } from "react-native"
 import LifeStatus from "../../Components/Common/LifeStatus"
+import StatusBar from "../../Components/Home/StatusBar"
 
 export default function Home(){ 
     const navigation = useNavigation()
@@ -15,6 +16,7 @@ export default function Home(){
                 <View style={{alignItems: "center"}}>
                     <Text style={styles.dailyChecks}> 20 dias - 80 checks</Text>
                     <LifeStatus />
+                    <StatusBar />
                 </View>
                 <Text
                  style={styles.explanationText}
