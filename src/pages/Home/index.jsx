22ca353e228613004cfc1a66e0ce5f0b3afe87f4
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2022-12-04 11:51:08 -0300

    Edit and created habit!

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import { ScrollView, Text, View, StyleSheet } from "react-native"
 import LifeStatus from "../../Components/Common/LifeStatus"
 import StatusBar from "../../Components/Home/StatusBar"
 import CreateHabit from "../../Components/Home/CreateHabit"
+import EditHabit from "../../Components/Home/EditHabit"
 
 export default function Home(){ 
     const navigation = useNavigation()
@@ -21,8 +22,57 @@ export default function Home(){
                 <View style={{alignItems: "center"}}>
                     <Text style={styles.dailyChecks}> 20 dias - 80 checks</Text>
                     <LifeStatus />
-                    <StatusBar />
-                    <CreateHabit habitArea="Mente" borderColor = "#ffff" />
+                    <StatusBar />              
+                    {/* inicio validação ou criação de hábitos da mente    */}
+                    {mindHabit ? (
+                        <EditHabit 
+                            habit={mindHabit?.habitName}
+                            frequency={`${mindHabit?.habitTime} - ${mindHabit?.habitFrequency}` }
+                            habitArea={mindHabit?.habitArea}
+                            checkColor="#90b7f3"
+                        />
+                    ):
+                    <CreateHabit habitArea="Mente" borderColor="#90b7f3"/>                                      
+                    }
+
+                    {/* inicio validação ou criação de hábitos da financeiros    */}
+
+                    {moneyHabit ? (
+                        <EditHabit 
+                            habit={moneyHabit?.habitName}
+                            frequency={`${moneyHabit?.habitTime} - ${moneyHabit?.habitFrequency}` }
+                            habitArea={moneyHabit?.habitArea}
+                            checkColor="#85BB65"
+                        />
+                    ):
+                    <CreateHabit habitArea="Financeiro" borderColor="#85BB65"/>                                      
+                    }
+
+                    {/* inicio validação ou criação de hábitos da Saúde    */}
+
+                    {bodyHabit ? (
+                        <EditHabit 
+                            habit={bodyHabit?.habitName}
+                            frequency={`${bodyHabit?.habitTime} - ${bodyHabit?.habitFrequency}` }
+                            habitArea={bodyHabit?.habitArea}
+                            checkColor="#FF0044"
+                        />
+                    ):
+                    <CreateHabit habitArea="Saúde" borderColor="#FF0044"/>                                      
+                    }
+
+                    {/* inicio validação ou criação de hábitos da Diversão    */}
+
+                    {funHabit ? (
+                        <EditHabit 
+                            habit={funHabit?.habitName}
+                            frequency={`${funHabit?.habitTime} - ${funHabit?.habitFrequency}` }
+                            habitArea={funHabit?.habitArea}
+                            checkColor="#FE7F23"
+                        />
+                    ):
+                    <CreateHabit habitArea="Humor" borderColor="#FE7F23"/>                                      
+                    }                                                           
                 </View>
                 <Text
                  style={styles.explanationText}
